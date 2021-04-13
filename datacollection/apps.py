1eae00c034a6b8200f76d4728b64b7004efdf035
PythonicNoob <atulsyan850@gmail.com>
2021-04-13 21:12:33 +0530

    Initial commit

---
+++ datacollection/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DatacollectionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'datacollection'
---
+++ datacollection/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, "index.html")
+
+def upload(request):
+    return render(request, "upload.html")
